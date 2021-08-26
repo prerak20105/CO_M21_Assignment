8a3c271410513690b9348ff242c24284e9c9867b
abhinav20012 <88596613+abhinav20012@users.noreply.github.com>
2021-08-26 21:49:52 +0530

    Update sc.py

--- SimpleSimulator/sc.py
+++ SimpleSimulator/sc.py
@@ -284,6 +284,22 @@ def movB_instruction(current_instruction):                        # @ ABHINAV
     register_values[register_code[reg1_code]]=decimal_to_list(res)
 
 
+def movC_instruction(current_instruction):                     # @ ABHINAV
+    global pc
+    global memory
+    global register_values
+    global register_code
+    global opcode
+    s=""
+    for x in current_instruction:
+        s+=str(x)
+    reg1_code=s[10:13]
+    reg2_code=s[13:]
+    val2=list_to_decimal( register_values[register_code[reg2_code]] )
+    res=val2
+    register_values[register_code[reg1_code]]=decimal_to_list(res)
+
+
 
 			
 			
